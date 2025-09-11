4fd990f4462daaea17bab94150efa042f4654e11
jakubstrunc <kubaelis@gmail.com>
2025-09-11 12:29:43 +0200

    added num showing

--- charLED.py
+++ charLED.py
@@ -1,9 +1,26 @@
+###
+num = "0"
 class charLED:
     
+    digit_map = [
+        [(0,-2), (1,-2), (2,-2), (3,-2)],
+        [(0,-1), (1,-1), (2,-1), (3,-1)],
+        [(0,0), (1,0), (2,0)],
+        [(-1,1), (0,1), (1,1), (2,1)],
+        [(-2,2), (-1,2), (0,2), (1,2)]
+    ]
+        
     char_map = {
-    "0": [(0,0),(1,0),(2,0),(0,1),(2,1),(0,2),(2,2),(0,3),(2,3),(0,4),(1,4),(2,4)],
-    "1": [(0,2),(-1,1)],
-    "2": [(0,0),(1,0),(2,0),(2,1),(1,2),(0,3),(0,4),(1,4),(2,4)]
+    "0": [digit_map[0][0], digit_map[0][1], digit_map[1][0], digit_map[1][2], digit_map[3][1], digit_map[3][3], digit_map[4][2], digit_map[4][3]],
+    "1": [digit_map[0][1], digit_map[1][1], digit_map[2][2], digit_map[3][2], digit_map[4][3]],
+    "2": [digit_map[0][0], digit_map[0][1], digit_map[1][2], digit_map[2][1], digit_map[2][2], digit_map[3][1], digit_map[4][2], digit_map[4][3]],
+    "3": [digit_map[0][0], digit_map[0][1], digit_map[1][2], digit_map[2][1], digit_map[2][2], digit_map[3][3], digit_map[4][2], digit_map[4][3]],
+    "4": [digit_map[0][0], digit_map[1][0], digit_map[1][2], digit_map[2][0], digit_map[2][1], digit_map[2][2], digit_map[3][2], digit_map[4][3]],
+    "5": [digit_map[0][0], digit_map[0][1], digit_map[1][0], digit_map[2][0], digit_map[2][1], digit_map[2][2], digit_map[3][3], digit_map[4][2], digit_map[4][3]],
+    "6": [digit_map[0][1], digit_map[1][1], digit_map[2][1], digit_map[2][2], digit_map[3][1], digit_map[3][3], digit_map[4][2], digit_map[4][3]],
+    "7": [digit_map[0][0], digit_map[0][1], digit_map[1][2], digit_map[2][2], digit_map[3][2], digit_map[4][2]],
+    "8": [digit_map[0][0], digit_map[0][1], digit_map[1][0], digit_map[1][2], digit_map[2][1], digit_map[2][2], digit_map[3][1], digit_map[3][3], digit_map[4][2], digit_map[4][3]],
+    "9": [digit_map[0][0], digit_map[0][1], digit_map[1][0], digit_map[1][2], digit_map[2][1], digit_map[2][2], digit_map[3][2], digit_map[4][2]]
     }
     def __init__(self, x, y, np, led_map):
         self.x = x  # levý horní roh znaku
@@ -17,7 +34,7 @@ class charLED:
         char = str(char)
         
         coords = self.char_map.get(char)
-        print(coords)
+        # print(coords)
 
         if not coords:
             print("error")
@@ -25,9 +42,3 @@ class charLED:
         for dx, dy in coords:
             self.np[self.led_map[self.y + dy][self.x + dx]] = (255, 0, 0)
         self.np.write()
-
-        
-
-    
-
-
